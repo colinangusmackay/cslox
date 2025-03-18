fcb826fd568cd6aef04019fcc5f461c5a72932e0
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-03-18 23:00:25 +0000

    5.4 A (not very) pretty printer

---
+++ src/cslox/AstPrinter.cs
@@ -0,0 +1,37 @@
+using System.Text;
+using cslox.Expressions;
+
+namespace cslox;
+
+public class AstPrinter : IVisitor<string>
+{
+    public string Print(Expr expr)
+        => expr.Accept(this);
+
+    public string VisitBinaryExpr(Binary binary)
+        => Parenthesise(binary.Operator.Lexeme, binary.Left, binary.Right);
+
+    public string VisitGroupingExpr(Grouping grouping)
+        => Parenthesise("group", grouping.Expression);
+
+    public string VisitLiteralExpr(Literal literal)
+        => literal.Value?.ToString() ?? "nil";
+
+    public string VisitUnaryExpr(Unary unary)
+        => Parenthesise(unary.Operator.Lexeme, unary.Right);
+
+    private string Parenthesise(string op, params Expr[] exprs)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append('(');
+        sb.Append(op);
+        foreach (var expr in exprs)
+        {
+            sb.Append(' ');
+            sb.Append(expr.Accept(this));
+        }
+
+        sb.Append(')');
+        return sb.ToString();
+    }
+}
