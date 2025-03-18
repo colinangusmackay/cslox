6ed323bd0cb32666bfdd0fe853b5c6a8b3baf5cc
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-03-18 21:01:40 +0000

    5.2.2 Metaprogramming the trees

---
+++ Generate-Ast.ps1
@@ -0,0 +1,76 @@
+$outputDir = "$PSScriptRoot/src/cslox";
+
+function DefineAst{ param([string]$baseName, [object[]]$types)
+    $path = "$outputDir/$baseName.cs";
+
+    Write-Host "Generating $path";
+
+    "// AUTO GENERATED FILE" | Out-File $path -Encoding utf8 -Force;
+    "" | Out-File $path -Encoding utf8 -Append;
+    "public abstract class $baseName" | Out-File $path -Encoding utf8 -Append;
+    "{" | Out-File $path -Encoding utf8 -Append;
+
+    foreach ($type in $types) {
+        $typeParts = $type -split ":";
+        $className = $typeParts[0].Trim();
+        $fields = $typeParts[1].Trim();
+
+        DefineType -path $path -baseName $baseName -className $className -fieldList $fields;
+    }
+
+    "}" | Out-File $path -Encoding utf8 -Append;
+}
+
+function DefineType{ param ([string]$path, [string]$baseName, [string]$className, [object[]]$fieldList)
+    "" | Out-File $path -Encoding utf8 -Append;
+    "    public class $className : $baseName" | Out-File $path -Encoding utf8 -Append;
+    "    {" | Out-File $path -Encoding utf8 -Append;
+
+    $fields = $fieldList -split ", ";
+
+    "        public $classname(" | Out-File $path -Encoding utf8 -Append -NoNewline;
+    $isFirst = $true;
+    foreach ($field in $fields) {
+        $fieldParts = $field -split " ";
+        $type = $fieldParts[0].Trim();
+        $name = $fieldParts[1].Trim();
+        $argName = $name.ToLowerInvariant();
+        if ($isFirst) {
+            $isFirst = $false;
+        } else {
+            ", " | Out-File $path -Encoding utf8 -Append -NoNewline;
+        }
+        "$type $argName" | Out-File $path -Encoding utf8 -Append -NoNewline;
+    }
+    ")" | Out-File $path -Encoding utf8 -Append;
+
+    "        {" | Out-File $path -Encoding utf8 -Append;
+    foreach ($field in $fields) {
+        $fieldParts = $field -split " ";
+        $propertyName = $fieldParts[1].Trim();
+        $argName = $propertyName.ToLowerInvariant();
+        "            $propertyName = $argName;" | Out-File $path -Encoding utf8 -Append;
+    }
+    "        }" | Out-File $path -Encoding utf8 -Append;
+
+    foreach ($field in $fields) {
+        $fieldParts = $field -split " ";
+        $type = $fieldParts[0].Trim();
+        $name = $fieldParts[1].Trim();
+
+        "" | Out-File $path -Encoding utf8 -Append;
+        "        public $type $name { get; }" | Out-File $path -Encoding utf8 -Append;
+    }
+
+    "    }" | Out-File $path -Encoding utf8 -Append;
+}
+
+Clear-Host
+DefineAst -baseName "Expr" -types @(
+    "Binary   : Expr Left, Token Operator, Expr Right",
+    "Grouping : Expr Expression",
+    "Literal  : object Value",
+    "Unary    : Token Operator, Expr Right"
+    );
+
+
---
+++ src/cslox/Expr.cs
@@ -0,0 +1,54 @@
+// AUTO GENERATED FILE
+
+public abstract class Expr
+{
+
+    public class Binary : Expr
+    {
+        public Binary(Expr left, Token operator, Expr right)
+        {
+            Left = left;
+            Operator = operator;
+            Right = right;
+        }
+
+        public Expr Left { get; }
+
+        public Token Operator { get; }
+
+        public Expr Right { get; }
+    }
+
+    public class Grouping : Expr
+    {
+        public Grouping(Expr expression)
+        {
+            Expression = expression;
+        }
+
+        public Expr Expression { get; }
+    }
+
+    public class Literal : Expr
+    {
+        public Literal(object value)
+        {
+            Value = value;
+        }
+
+        public object Value { get; }
+    }
+
+    public class Unary : Expr
+    {
+        public Unary(Token operator, Expr right)
+        {
+            Operator = operator;
+            Right = right;
+        }
+
+        public Token Operator { get; }
+
+        public Expr Right { get; }
+    }
+}
