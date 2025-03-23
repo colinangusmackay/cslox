510393f1e30c73f49088a0ba4fa0563a0c16168a
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-03-23 21:59:00 +0000

    8.3 Environments

---
+++ src/cslox/Environment.cs
@@ -0,0 +1,20 @@
+namespace cslox;
+
+public class Environment
+{
+    private readonly Dictionary<string, object?> _values = new();
+    
+    public void Define(string name, object? value)
+    {
+        _values[name] = value;
+    }
+
+    public object? Get(Token name)
+    {
+        if (_values.TryGetValue(name.Lexeme, out var value))
+        {
+            return value;
+        }
+        throw new RuntimeException(name, $"Undefined variable '{name.Lexeme}'.");
+    }
+}
