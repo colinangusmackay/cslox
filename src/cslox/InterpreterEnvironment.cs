c461989ee793bf25953ae17d2e5efc0a1696fae2
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-03-25 22:48:00 +0000

    8.5.1 Nesting and shadowing

--- src/cslox/InterpreterEnvironment.cs
+++ src/cslox/InterpreterEnvironment.cs
@@ -2,8 +2,19 @@ namespace cslox;
 
 public class InterpreterEnvironment
 {
+    private readonly InterpreterEnvironment? _enclosing;
     private readonly Dictionary<string, object?> _values = new();
-    
+
+    public InterpreterEnvironment()
+    {
+        _enclosing = null;
+    }
+
+    public InterpreterEnvironment(InterpreterEnvironment enclosing)
+    {
+        _enclosing = enclosing;
+    }
+
     public void Define(string name, object? value)
     {
         _values[name] = value;
@@ -15,6 +26,12 @@ public class InterpreterEnvironment
         {
             return value;
         }
+
+        if (_enclosing != null)
+        {
+            return _enclosing.Get(name);
+        }
+
         throw new RuntimeException(name, $"Undefined variable '{name.Lexeme}'.");
     }
 
@@ -23,6 +40,12 @@ public class InterpreterEnvironment
         if (_values.TryGetValue(name.Lexeme, out var oldValue))
         {
             _values[name.Lexeme] = value;
+            return;
+        }
+
+        if (_enclosing != null)
+        {
+            _enclosing.Assign(name, value);
             return;
         }
 
